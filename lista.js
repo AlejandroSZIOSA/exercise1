f0e870feba431e902dcd68e37163f164e97fda46
Gabriel Alejandro . S <90352812+AlejandroSZIOSA@users.noreply.github.com>
2024-08-21 16:58:47 +0200

    Fix Delete Btn Event Listener

--- lista.js
+++ lista.js
@@ -1,23 +1,23 @@
 const inputEl = document.getElementById("textInput");
 const ulEl = document.getElementById("listBody");
 
-btn.addEventListener("click", (e) => {
+btn.addEventListener("click", function () {
   let input = inputEl.value;
 
   //Creating new elements
   const newItem = document.createElement("li");
   newItem.innerHTML = input;
   const newBtn = document.createElement("button");
-  newBtn.innerHTML = "Remove";
+  newDeleteBtn.innerHTML = "Remove";
 
   //Adding new elements
   ulEl.appendChild(newItem);
   newItem.appendChild(newBtn);
 
-  //Event listener remove (nested event listeners)
-  newItem.addEventListener("click", function () {
-    this.remove();
-  });
+  //Nested Event listener
+  newDeleteBtn.onclick = function () {
+    newItem.remove();
+  };
 
   //Cleaning Input Element
   inputEl.value = "";
